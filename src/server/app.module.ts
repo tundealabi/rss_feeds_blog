992336c503b57cd0f2755e8bb7bd70949fb917c8
Tunde Alabi <tundealabi4780@gmail.com>
2022-05-13 00:15:12 +0100

    configure mongo db

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ViewModule } from './view/view.module';
@@ -8,6 +9,15 @@ import { ViewModule } from './view/view.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          uri: configService.get('DATABASE_URI'),
+        };
+      },
     }),
     ViewModule,
   ],
